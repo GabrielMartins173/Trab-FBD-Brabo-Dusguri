051cbc0a7cdc160bac76b5e304c178610c7a9e07
Gabriel Martins dos Santos <gabrielmartins173@gmail.com>
2019-12-11 00:37:16 -0300

    eh usguri

--- Skype/src/skype/DataBase.java
+++ Skype/src/skype/DataBase.java
@@ -4,9 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 
 public class DataBase {
+
+    private static int chatId = 6;
+    private static int mensagemId = 9;
+    private static int chamadaId = 2;
+    Scanner scan = new Scanner(System.in);
 
     private Connection connection = null;
     private Statement statement = null;
@@ -38,156 +44,23 @@ public class DataBase {
             return false;
     }
 
-//    public void getSongs() {
-//        try {
-//            String query = "select nomeMidia from Midia;";
-//            this.resultset = this.statement.executeQuery(query);
-//            this.statement = this.connection.createStatement();
-//            while (this.resultset.next())
-//                System.out.println("nome da midia: " + this.resultset.getString("nomeMidia"));
-//            System.out.println("\n\n");
-//            ;
-//        } catch (Exception e) {
-//            System.out.println("Erro: " + e.getMessage());
-//        }
-//    }
-//
-//    public void playlistInfo() {
-//        try {
-//            String query = "select nomePlaylist,count(*) as numeroDeMusicas, sum(duracaoMinutosMidia)from playlist natural join playListMusica natural join midia group by nomePlaylist;";
-//            this.resultset = this.statement.executeQuery(query);
-//            this.statement = this.connection.createStatement();
-//            while (this.resultset.next())
-//                System.out.println("Nome da playlist: " + this.resultset.getString("nomePlaylist") +
-//                        "\nNumero de musicas: " + this.resultset.getString("numeroDeMusicas") +
-//                        "\nDuracao da playlist: " + this.resultset.getString("sum(duracaoMinutosMidia)") + "minutos" + '\n');
-//            System.out.println();
-//            System.out.println();
-//            ;
-//        } catch (Exception e) {
-//            System.out.println("Erro: " + e.getMessage());
-//        }
-//    }
-//
-//    public void topSongs() {
-//        try {
-//            String query = "select nomeArtista, nomeMidia, count(codUsuario) as NumeroDeVezesTocada\r\n" +
-//                    "from Musica Natural join Midia natural join UsuarioMidia natural join artista\r\n" +
-//                    "where ehMusica = true\r\n" +
-//                    "group by nomeMidia\r\n" +
-//                    "order by (count(codUsuario)) DESC;";
-//            this.resultset = this.statement.executeQuery(query);
-//            this.statement = this.connection.createStatement();
-//            while (this.resultset.next())
-//                System.out.println("Nome da Artista: " + this.resultset.getString("nomeArtista") +
-//                        "\nNome da Midia: " + this.resultset.getString("nomeMidia") +
-//                        "\nNumero de reproducoes: " + this.resultset.getString("numeroDeVezesTocada") + '\n');
-//            System.out.println();
-//            System.out.println();
-//            ;
-//        } catch (Exception e) {
-//            System.out.println("Erro: " + e.getMessage());
-//        }
-//    }
-//
-//    public void nextShows() {
-//        try {
-//            String query = "select distinct NomeApresentacao, cidadeLugar, dataApresentacao\r\n" +
-//                    "from artista natural join apresentacao natural join lugar\r\n" +
-//                    "where lugar.paisLugar = 'Brasil';";
-//            this.resultset = this.statement.executeQuery(query);
-//            this.statement = this.connection.createStatement();
-//            while (this.resultset.next())
-//                System.out.println("Nome da Apresentacao: " + this.resultset.getString("nomeApresentacao") +
-//                        "\nCidade: " + this.resultset.getString("cidadeLugar") +
-//                        "\nData: " + this.resultset.getString("dataApresentacao") + '\n');
-//            System.out.println();
-//            System.out.println();
-//            ;
-//        } catch (Exception e) {
-//            System.out.println("Erro: " + e.getMessage());
-//        }
-//    }
-//
-//    public void notLinkin() {
-//        try {
-//            String query = "	select distinct nomeArtista\r\n" +
-//                    "	from Artista a1\r\n" +
-//                    "	Where NOT EXISTS (select * \r\n" +
-//                    "		From Artista natural join ArtistaApresentacao\r\n" +
-//                    "		Where codArtista = a1.codArtista and\r\n" +
-//                    "	 	codApresentacao IN (select distinct codApresentacao\r\n" +
-//                    "		From apresentacao natural join ArtistaApresentacao natural join Artista\r\n" +
-//                    "		Where nomeArtista = 'Linkin Park'));\r\n" +
-//                    "	;";
-//            this.resultset = this.statement.executeQuery(query);
-//            this.statement = this.connection.createStatement();
-//            while (this.resultset.next())
-//                System.out.println("Nome do Artista: " + this.resultset.getString("nomeArtista") +
-//                        '\n');
-//            System.out.println();
-//            System.out.println();
-//            ;
-//        } catch (Exception e) {
-//            System.out.println("Erro: " + e.getMessage());
-//        }
-//    }
-//
-//    public void albumsInfo() {
-//        try {
-//            String query = "select nomeArtista,tituloAlbum, nomeMidia, ordemFaixa\r\n" +
-//                    "from musica natural join midia natural join Album natural join albumMusica natural join Artista\r\n" +
-//                    "order by tituloAlbum,ordemFaixa;";
-//            this.resultset = this.statement.executeQuery(query);
-//            this.statement = this.connection.createStatement();
-//            while (this.resultset.next())
-//                System.out.println("Nome do Artista: " + this.resultset.getString("nomeArtista") +
-//                        "\nTitulo do album: " + this.resultset.getString("tituloAlbum") +
-//                        "\nnnomeMusica: " + this.resultset.getString("nomeMidia") +
-//                        "\nOrdem faixa:: " + this.resultset.getString("ordemFaixa") + '\n');
-//            System.out.println();
-//            System.out.println();
-//            ;
-//        } catch (Exception e) {
-//            System.out.println("Erro: " + e.getMessage());
-//        }
-//    }
-//
-//    public void codUsersdontPodcast() {
-//        try {
-//            String query = "select distinct codUsuario, nomeu\r\n" +
-//                    "from usuario\r\n" +
-//                    "where codUsuario not in ( select codUsuario\r\n" +
-//                    "							from UsuarioMidia natural join Podcast natural join Midia\r\n" +
-//                    "							where ehMusica = false);";
-//            this.resultset = this.statement.executeQuery(query);
-//            this.statement = this.connection.createStatement();
-//            while (this.resultset.next())
-//                System.out.println("Codigo do usuario: " + this.resultset.getString("codUsuario") +
-//                        "\nNome usuario: " + this.resultset.getString("nomeu") + '\n');
-//            System.out.println();
-//            System.out.println();
-//        } catch (Exception e) {
-//            System.out.println("Erro: " + e.getMessage());
-//        }
-//    }
 
     public boolean verifyLogin(String nome, String password) {
 
         try {
-			String query = "select distinct nome, senha\r\n" +
-					"from usuario;";
+            String query = "select distinct nome, senha\r\n" +
+                    "from usuario;";
 
-			this.resultset = this.statement.executeQuery(query);
-			this.statement = this.connection.createStatement();
-			while (this.resultset.next()){
+            this.resultset = this.statement.executeQuery(query);
+            this.statement = this.connection.createStatement();
+            while (this.resultset.next()) {
 
-				if(this.resultset.getString("nome").equals(nome) && this.resultset.getString("senha").equals(password)){
-					return true;
-				}else {
-					return false;
-				}
-			}
+                if (this.resultset.getString("nome").equals(nome) && this.resultset.getString("senha").equals(password)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
             return false;
@@ -195,23 +68,213 @@ public class DataBase {
         return false;
     }
 
-	public String getIdByName(String nome) {
+    public String getIdByName(String in) {
 
-        try{
-            String query = "select usuario_id\r\n" +
+        try {
+            String query = "select usuario_id, nome\r\n" +
                     "from usuario\r\n" +
-                    "where nome = " + nome + ";";
+                    "where nome ='" + in + "';";
             this.resultset = this.statement.executeQuery(query);
             this.statement = this.connection.createStatement();
-            while(this.resultset.next()){
-                if(this.resultset.getString("usuario_id").equals(nome)){
+            while (this.resultset.next()) {
+                if (this.resultset.getString("nome").equals(in)) {
                     return this.resultset.getString("usuario_id");
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
 
         return "Id não encontrado :( ";
     }
+
+    public void listaContatos(String id) {
+
+        try {
+            String query = "select distinct ldc.nome, u.* from usuario_simples dono\r\n" +
+                    "join lista_de_contatos ldc on dono.usuario_id = dono\r\n" +
+                    "join usuario_simples u on u.usuario_id = ldc.usuario_id\r\n" +
+                    "where dono.usuario_id = '" + id + "'\r\n" +
+                    "order by ldc.nome;";
+            this.resultset = this.statement.executeQuery(query);
+            this.statement = this.connection.createStatement();
+            while (this.resultset.next())
+                System.out.println("Nome da Lista de Contatos: " + this.resultset.getString("ldc.nome") +
+                        "\nId: " + this.resultset.getString("u.usuario_id") +
+                        "\nNome: " + this.resultset.getString("u.nome") +
+                        "\nE-mail: " + this.resultset.getString("u.email") + '\n');
+            System.out.println();
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    public void olhaCalendario(String id) {
+
+        try {
+            String query = "select u.nome, c.* from usuario u\r\n" +
+                    "join calendario cal using(usuario_id)\r\n" +
+                    "join compromisso_reuniao c using(compromisso_id)\r\n" +
+                    "where u.usuario_id = '" + id + "';";
+            this.resultset = this.statement.executeQuery(query);
+            this.statement = this.connection.createStatement();
+            while (this.resultset.next())
+                System.out.println("Nome: " + this.resultset.getString("u.nome") +
+                        "\nId Reunião: " + this.resultset.getString("c.reuniao_id") +
+                        "\nId Compromisso: " + this.resultset.getString("c.compromisso_id") +
+                        "\nInício: " + this.resultset.getString("c.data_de_inicio") +
+                        "\nTérmino: " + this.resultset.getString("c.data_de_termino") +
+                        "\nDescrição: " + this.resultset.getString("c.descricao") +
+                        "\nNome do Compromisso: " + this.resultset.getString("c.nome") +
+                        "\nLocal: " + this.resultset.getString("c.sala_de_reuniao") +
+                        "\nId Outro Participante: " + this.resultset.getString("c.usuario_id") + '\n');
+            System.out.println();
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    public void usarChat(String id) {
+
+        String idSelecionado;
+        int numeroParticipantes = 2;
+
+        try {
+            String query = "SELECT CHAT_ID, NUMERO_DE_PARTICIPANTES, NOME FROM CHAT JOIN USUARIO_SIMPLES USING(USUARIO_ID)\r\n" +
+                    "WHERE CHAT_ID IN (SELECT DISTINCT CHAT_ID FROM CHAT\r\n" +
+                    "WHERE USUARIO_ID = " + id + ");";
+
+            this.resultset = this.statement.executeQuery(query);
+            this.statement = this.connection.createStatement();
+
+            while (this.resultset.next()) {
+                System.out.println("Id: " + this.resultset.getString("chat_id") +
+                        "\nNúmero Participantes: " + this.resultset.getString("numero_de_participantes") +
+                        "\nNome: " + this.resultset.getString("nome"));
+            }
+
+            System.out.println("Selecione o Id do chat que deseja usar.");
+
+            idSelecionado = scan.nextLine();
+
+            String query2 = "select distinct numero_de_participantes from chat where chat_id = " + idSelecionado + ";";
+
+            this.resultset = this.statement.executeQuery(query2);
+            this.statement = this.connection.createStatement();
+
+            this.resultset.next();
+            numeroParticipantes = resultset.getInt("numero_de_participantes");
+
+
+            chat(Integer.parseInt(idSelecionado), numeroParticipantes, id);
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    public void criarChat(String id) {
+        try {
+
+            int numeroParticipantes = 2;
+            int idSelecionado = 1;
+
+            idSelecionado = selecionaUsuario(id);
+
+            String query2 = "INSERT INTO CHAT (CHAT_ID, NUMERO_DE_PARTICIPANTES,USUARIO_ID, PRIVILEGIO_ADMIN)\r\n" +
+                    "VALUES(" + chatId + "," + numeroParticipantes + ", " + id + ", TRUE);";
+
+            this.statement.executeUpdate(query2);
+            this.statement = this.connection.createStatement();
+
+            String query3 = "INSERT INTO CHAT (CHAT_ID, NUMERO_DE_PARTICIPANTES,USUARIO_ID, PRIVILEGIO_ADMIN)\r\n" +
+                    "VALUES(" + chatId + "," + numeroParticipantes + ", " + idSelecionado + ", FALSE);";
+
+            this.statement.executeUpdate(query3);
+            this.statement = this.connection.createStatement();
+
+            System.out.println("Chat Criado !");
+
+            chatId++;
+
+            chat(chatId - 1, numeroParticipantes, id);
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    private void chat(int idChat, int participantes, String idLogado) throws Exception {
+        int x = 7;
+        int userId;
+
+        while (x!= 3) {
+            System.out.println("Selecione o que voce desejar fazer com o chat:");
+            System.out.println("1 - Adicionar usuario ao chat.");
+            System.out.println("2 - Escrever Mensagem.");
+            System.out.println("3 - Sair.");
+
+            x = scan.nextInt();
+
+            if(x==1){
+
+                userId = selecionaUsuario(idLogado);
+
+                String query = "INSERT INTO CHAT (CHAT_ID, NUMERO_DE_PARTICIPANTES,USUARIO_ID, PRIVILEGIO_ADMIN)\r\n" +
+                        "VALUES("+ idChat +", "+ participantes +","+ userId +", FALSE);";
+
+                this.statement.executeUpdate(query);
+                this.statement = this.connection.createStatement();
+
+                participantes++;
+
+                String query2 = "UPDATE CHAT SET NUMERO_DE_PARTICIPANTES = 3 WHERE CHAT_ID = "+ idChat +";";
+                this.statement.executeUpdate(query2);
+                this.statement = this.connection.createStatement();
+
+                System.out.println("Usuario adicionado com Sucesso!");
+
+            }else if(x==2){
+
+                System.out.println("Digite qual mensagem deseja inserir:");
+
+                String mensagem = scan.nextLine();
+
+                String query = "INSERT INTO MENSAGEM(MENSAGEM_ID, CONTEUDO, CHAT_ID, ARQUIVO_ID, USUARIO_ID, DATA_DE_ENVIO)\r\n" +
+                        "VALUES("+mensagemId+",'"+ mensagem + "',"+ idChat +", null, "+ idLogado +", CURDATE());";
+
+                this.statement.executeUpdate(query);
+                this.statement = this.connection.createStatement();
+
+                mensagemId++;
+
+                System.out.println("Mensagem inserida com sucesso!");
+
+            }else{
+                System.out.println("Escolha uma opção válida ! (1,2 ou 3).");
+            }
+        }
+
+
+    }
+
+    private int selecionaUsuario(String id) throws Exception {
+        String queryListUsers = "SELECT * FROM USUARIO_SIMPLES\r\n" +
+                "WHERE USUARIO_ID <> " + id + ";";
+
+        this.resultset = this.statement.executeQuery(queryListUsers);
+        this.statement = this.connection.createStatement();
+
+        System.out.println("Selecione o id do usuário com o qual gostaria de iniciar um chat.");
+
+        while (this.resultset.next()) {
+            System.out.println("Id: " + this.resultset.getString("usuario_id") +
+                    "\nNome: " + this.resultset.getString("nome") + '\n');
+        }
+
+        return scan.nextInt();
+    }
+
 }
--- Skype/src/skype/Skype.java
+++ Skype/src/skype/Skype.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Skype {
 
+
     private static DataBase db = new DataBase();
 
     public static void main(String[] args) {
@@ -33,35 +34,44 @@ public class Skype {
 
                     id_logado = db.getIdByName(nome);
 
-                    System.out.println(id_logado);
-
                     if (verifyLogin(nome, password)) {
-                        while (x != 0) {
+                        while (x != 7) {
                             System.out.println("1 - Criar Chat com Usuário.");
-                            System.out.println("2 - Criar um Compromisso.");
-                            System.out.println("3 - Criar uma Reunião.");
+                            System.out.println("2 - Listar todos os seus contatos.");
+                            System.out.println("3 - Olhar o seu calendário.");
                             System.out.println("4 - Criar Chat Privado.");
                             System.out.println("5 - Olhar Lista de Contatos.");
                             System.out.println("6 - Comprar Assinatura Premium.");
-                            System.out.println("0 para sair ");
+                            System.out.println("7 - Deslogar. ");
                             System.out.println("Digite o que voce quer fazer: ");
                             x = scan.nextInt();
 
                             if (db.isConnected()) {
                                 if (x == 1) {
                                     System.out.println("Escolha o que você deseja fazer nesse chat:");
+                                    int y;
+                                    System.out.println("1 - Usar Chat Existente.");
+                                    System.out.println("2 - Criar Novo Chat.");
 
-                                    System.out.println("1");
+                                    y = scan.nextInt();
+
+                                    if(y==1){
+                                        db.usarChat(id_logado);
+                                    }else if (y==2){
+                                        db.criarChat(id_logado);
+                                    }
                                 } else if (x == 2)
-                                    System.out.println("1");
+                                    db.listaContatos(id_logado);
                                 else if (x == 3)
-                                    System.out.println("1");
+                                    db.olhaCalendario(id_logado);
                                 else if (x == 4)
                                     System.out.println("1");
                                 else if (x == 5)
                                     System.out.println("1");
                                 else if (x == 6)
                                     System.out.println("1");
+                                else if (x == 7)
+                                    System.out.println("Deslogando...");
                             } else {
                                 System.out.println("Não foi possível realizar a conexão com o Banco de Dados. ");
 
